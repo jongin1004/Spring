32944bf3293879762a6b5e12bc5cbba368ef5eaa
jongin1004 <bill1224@naver.com>
2021-10-20 21:22:31 +0900

    get params and show value

--- Study/src/main/java/hello/hellospring/controller/helloController.java
+++ Study/src/main/java/hello/hellospring/controller/helloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
@@ -11,5 +12,12 @@ public class helloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    // model에 담으면 view에서 렌더링할 때 사용되는 것
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
