9430828f2a60892b26cfd80c338190ce8e803ce1
jongin1004 <bill1224@naver.com>
2021-10-21 20:20:29 +0900

    컴포넌트 스캔과 자동 의존관계 설정

---
+++ Study/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+
+//Controller 어노테이션이 붙어있으면, 스프링 서버거 시작될 때 스프링 컨테이너에서 해당 클래스를 가지고 관리하게 됩니다.
+//그러면, new ()를 이용해서 객체를 생성하는 것 보다는 스프링 컨테이너에 있는 클래스(객체)와 연결해주는 것이 더욱 좋다. ( 부가 기능도 사용 가능)
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    //Autowired어노테이션은 스프링의 컨테이너에 있는 클래스와 연결을 하겠다는 의미
+    //MemberController가 생성이되면, 스프링 컨테이너에 있는 memberService를 가져와서 넣어줌 ( 연결한다.) = 의존관계 주입 = 의존관계 설정
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- Study/src/main/java/hello/hellospring/service/MemberService.java
+++ Study/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,12 +3,24 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+//@Service 어노테이션을 붙히는 이유는, @Autowired를 이용해서 스프링 컨테이너에 있는 클래스와 연결하고 싶은데, 아무 어노테이션도 붙어있지 않은
+//Java 클래스는 스프링에서 알 수가 없음. @Service가 있으면, 스프링 컨테이너가 스프링 컨테이너에서 관리할 수 있도록 등록합니다.
+@Service
 public class MemberService {
-    private final MemberRepository memberRepositroy = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    //Autowired어노테이션은 스프링의 컨테이너에 있는 클래스와 연결을 하겠다는 의미
+    //MemberService 생성이되면, 스프링 컨테이너에 있는 MemberRepository 가져와서 넣어줌 ( 연결한다.) = 의존관계 주입 = 의존관계 설정
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /*
     회원가입
@@ -21,7 +33,7 @@ public class MemberService {
     }
 
     private void validateDuplicateMember(Member member) {
-        memberRepositroy.findByName(member.getName())
+        memberRepository.findByName(member.getName())
             .ifPresent(m -> {
                 throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
@@ -29,10 +41,10 @@ public class MemberService {
 
     /*전체회원 조회*/
     public List<Member> findMembers() {
-        return memberRepositroy.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
-        return memberRepositroy.findById(memberId)
+        return memberRepository.findById(memberId);
     }
 }
