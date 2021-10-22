af9e63007212ed44927644d585d25f25f8093679
jongin1004 <bill1224@naver.com>
2021-10-22 15:25:59 +0900

    AOP

--- Study/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ Study/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -1,10 +1,10 @@
-package hello.hellospring.repository;
-
-import javax.sql.DataSource;
-
-public class JdbcMemberRepository implements MemberRepository {
-
-    //DB와 연결하기 위해서 DataSource가 필요
-    private final DataSource dataSource;
-
-}
+//package hello.hellospring.repository;
+//
+//import javax.sql.DataSource;
+//
+//public class JdbcMemberRepository implements MemberRepository {
+//
+//    //DB와 연결하기 위해서 DataSource가 필요
+//    private final DataSource dataSource;
+//
+//}
