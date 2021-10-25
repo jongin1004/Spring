6e50db36d1a106aebb7f38e315d5f1eea0be417a
jongin1004 <bill1224@naver.com>
2021-10-25 14:58:48 +0900

    이름과 타입 조회 그리고 예외 테스트

---
+++ stoudy_basisc/src/test/java/hello/core/beanfind/ApplicationContextBaiscFindTest.java
@@ -0,0 +1,37 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBaiscFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void beanFindByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    @DisplayName("빈 타입으로 조회")
+    void beanFindByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+    
+    @Test
+    @DisplayName("빈 없는 이름으로 조회 ")
+    void beanFindByName_x() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class));
+    }
+}
--- stoudy_basisc/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ stoudy_basisc/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -1,40 +1,40 @@
-package hello.core.beanfind;
-
-import hello.core.AppConfig;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class ApplicationContextInfoTest {
-
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-
-    @Test
-    @DisplayName("모든 빈 출력하기")
-    void findAllBean() {
-        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("name = " + beanDefinitionName + ", object = " + bean);
-        }
-    }
-
-    @Test
-    @DisplayName("모든 빈 출력하기")
-    void  findApplicationBean() {
-        //getBeanDefinitionNames() 스프링에 등록된 모든 빈 이름을 조회한다.
-        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            //getBeanDefinition() 빈의 대한 메타데이터 정보
-            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
-
-            // getRole()을 이용해서 빈을 구분할 수 있는데 (개발용, 스프링 내부에서 사용되는 용)
-            // ROLE_APPLICATION - 개발용
-            // ROLE_INFRASTRUCTURE - 스프링 내부용용            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
-                Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("name = " + beanDefinitionName + ", object = " + bean);
-            }
-        }
-    }
-}
+//package hello.core.beanfind;
+//
+//import hello.core.AppConfig;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.config.BeanDefinition;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//
+//public class ApplicationContextInfoTest {
+//
+//    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+//
+//    @Test
+//    @DisplayName("모든 빈 출력하기")
+//    void findAllBean() {
+//        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+//        for (String beanDefinitionName : beanDefinitionNames) {
+//            Object bean = ac.getBean(beanDefinitionName);
+//            System.out.println("name = " + beanDefinitionName + ", object = " + bean);
+//        }
+//    }
+//
+//    @Test
+//    @DisplayName("모든 빈 출력하기")
+//    void  findApplicationBean() {
+//        //getBeanDefinitionNames() 스프링에 등록된 모든 빈 이름을 조회한다.
+//        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+//        for (String beanDefinitionName : beanDefinitionNames) {
+//            //getBeanDefinition() 빈의 대한 메타데이터 정보
+//            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+//
+//            // getRole()을 이용해서 빈을 구분할 수 있는데 (개발용, 스프링 내부에서 사용되는 용)
+//            // ROLE_APPLICATION - 개발용
+//            // ROLE_INFRASTRUCTURE - 스프링 내부용용            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+//                Object bean = ac.getBean(beanDefinitionName);
+//                System.out.println("name = " + beanDefinitionName + ", object = " + bean);
+//            }
+//        }
+//    }
+//}
