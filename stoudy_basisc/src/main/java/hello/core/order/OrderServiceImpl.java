90ef79fd3024d29d85fc6bf5f06bb5a4ff76447c
jongin1004 <bill1224@naver.com>
2021-10-26 18:09:07 +0900

    롬복을 이용해 코드 간단화하기

--- stoudy_basisc/src/main/java/hello/core/order/OrderServiceImpl.java
+++ stoudy_basisc/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,24 +6,26 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl  implements  OrderService{
 
-    private MemberRepository memberRepository; //회원 찾기용
-    private DiscountPolicy discountPolicy; //할인 정책
+    private final MemberRepository memberRepository; //회원 찾기용
+    private final DiscountPolicy discountPolicy; //할인 정책
     //OrderServiceImpl은 추상 인터페이스인 DiscountPolicy에도 의존하고 있지만, 구체(구현) 클래스인 FixDiscount/ RateDiscount에도 의존하고 있음
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //고정 할인 정책
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
