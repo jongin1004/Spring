3fed57daf31899ff537532f71854612c6a4c305d
jongin1004 <bill1224@naver.com>
2021-11-06 18:08:11 +0900

    연관관계 주인

--- blog/src/main/java/com/cos/blog/model/Board.java
+++ blog/src/main/java/com/cos/blog/model/Board.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -30,9 +31,17 @@ public class Board {
     @ColumnDefault("0")
     private int count;
 
-    @ManyToOne // Many = board, one = user -> 한 명의 유저는 여러개의 게시글을 가질 수 있다.
+    // Many = board, one = user -> 한 명의 유저는 여러개의 게시글을 가질 수 있다.
+    //ManyToOne일 경우에는 FetchType의 기본값이 EAGER이기 때문에, 생략가능 
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="userId") // 실제 테이블에는 userId라는 속성이름으로 저장됨
     private User user; //DB는 오브젝트를 저장할 수 없기 때문에 id(FK)를 사용
+
+    //mappedBy 연관관계의 주인이 아닌다. (FK가 아닙니다.) DB에 칼럼을 만들지 마세요
+    //fetch = FetchType.EAGER -> 무조건 다 땡겨와라! / EAGER이 아닌, LAZY는 필요할 때만 가져와라 라는 의미
+    //OneToMany일 경우에는 FetchType의 기본 값이 LAZY이기 때문에, 설정해줘야함
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER)
+    private List<Reply> reply;
 
     @CreationTimestamp
     private Timestamp createDate;
