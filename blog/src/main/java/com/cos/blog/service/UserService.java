465087d7371cdc848d9b471f11b5c94992df4daa
jongin1004 <bill1224@naver.com>
2021-11-07 22:39:40 +0900

    회원가입 - 비지니스 로직(2) Service

--- blog/src/main/java/com/cos/blog/service/UserService.java
+++ blog/src/main/java/com/cos/blog/service/UserService.java
@@ -5,13 +5,17 @@ import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 // 컴포넌트 스캔을 통해서 Bean에 등록하도록
 @Service
 public class UserService {
     
     @Autowired
     private UserRepository userRepository;
-    
+
+    //아래 함수를 하나의 트랜젝션으로 묻어서, 성공하면 commit을 실패하면 rollback을 하도록
+    @Transactional
     public int join(User user) {
         try {
             userRepository.save(user);
@@ -20,7 +24,7 @@ public class UserService {
             e.printStackTrace();
             System.out.println("e.getMessage() = " + e.getMessage());
         }
-        
+
         return -1;
     }
 }
